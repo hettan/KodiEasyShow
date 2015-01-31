4f27dcd76be1a2456c8a9e03ff8553be92dbd4f1
Jacob <jacobholm89@gmail.com>
2015-01-31 18:41:44 +0100

    Now choosing show based on the total length of matched words with chars >4. If more than one then the show with shortest string length is choosed

--- kodiEasyShow.py
+++ kodiEasyShow.py
@@ -10,9 +10,50 @@ WORDS = show_scanner.get_words()
 
 kodi_controller = KodiController()
 
-def handle(text, mic, profile):
-    found = False
+#TODO implement an algortihm for determinating the most matched from given words.
+def most_matched(text):
     word_counter = {}
+    for word in WORDS:
+        #Skip words smaller then 4 chars
+        if(word < 4):
+            continue
+
+        if bool(re.search(r'\b%s\b'%(word), text, re.IGNORECASE)):
+            matched_shows = show_scanner.match_shows(word)
+            for show in matched_shows:
+                if show in word_counter:
+                    word_counter[show] += len(word)
+                else:
+                    word_counter[show] = len(word)
+    print word_counter
+
+    #Nothing found
+    if len(word_counter) == 0:
+        return None
+    
+    if len(matched_shows) > 1:
+        max_counter = 0
+        most_matched = None
+        for show, counter in word_counter.items():
+            if counter > max_counter:
+                max_counter = counter
+                most_matched = show
+            elif counter == max_counter:
+                #The string with least diff i.e. the shortest one matched
+                most_matched = min([most_matched, show], key=len)
+
+        return most_matched
+    else:
+        return matched_shows[0]
+        
+            
+    
+    
+
+
+def handle(text, mic, profile):
+    #found = False
+    """word_counter = {}
     for word in WORDS:
         if bool(re.search(r'\b%s\b'%(word), text, re.IGNORECASE)):
             matched_shows = show_scanner.match_shows(word)
@@ -33,6 +74,18 @@ def handle(text, mic, profile):
             kodi_controller.play_file(latest_episode)
         else:
             mic.say("No episodes found")
+    """
+    show = most_matched(text)
+    
+    if show::
+        latest_episode = show_scanner.find_latest_episode(most_matched_show)
+        if latest_episode:
+            kodi_controller.play_file(latest_episode)
+        else:
+            mic.say("No episodes found")
+
+    else:
+        mic.say("Not found")
 
 def isValid(text):
     search_shows = str.join("|", WORDS)
@@ -40,12 +93,16 @@ def isValid(text):
 
 #Not run on import
 def main():
-    show_word = "FAMILY"
-    shows = show_scanner.match_shows(show_word)
-    episode = show_scanner.find_latest_episode(shows[0])
-    kodi_controller.play_file(episode)
+    #show_word = "FAMILY"
+    #shows = show_scanner.match_shows(show_word)
+    #episode = show_scanner.find_latest_episode(shows[0])
+    show = most_matched(show_words)
+    episode = show_scanner.find_latest_episode(show)
+    print("found episode %s"%(episode))
+    #kodi_controller.play_file(episode)
 
 #Just for testing
 if __name__ == "__main__":
     main()
+
 
